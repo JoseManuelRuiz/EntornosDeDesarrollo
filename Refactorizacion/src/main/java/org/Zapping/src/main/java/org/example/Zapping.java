24c3576c65819f11a6223254fa3cab64b2180d12
JoseManuelRuiz <josruisoj@alu.edu.gva.es>
2026-04-22 13:31:51 +0200

    Creacion primer test y metodo con lo minimo para que pase el test 0

--- Refactorizacion/src/main/java/org/Zapping/src/main/java/org/example/Zapping.java
+++ Refactorizacion/src/main/java/org/Zapping/src/main/java/org/example/Zapping.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class Zapping {
+
+    public static int minClicks(int origen, int destino) {
+        int diferencia = Math.abs(origen - destino);
+        return Math.min(diferencia, 99 - diferencia);
+    }
 }
